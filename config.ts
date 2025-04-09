6db36636b70ff165954afbf742ec031bfdbb20f6
dofmin <dofmin0@naver.com>
2025-04-10 00:53:33 +0900

    req (#11)
    * docs(serpack): plugin api
    
    * feat: primary color
    
    * chore: update
    
    * chore: redirect
    
    * feat: build
    
    * serpack
    
    * docs:request

--- config.ts
+++ config.ts
@@ -120,6 +120,7 @@ export default [
           { build: 'Build Server' },
           { init: 'Init Project' },
           { start: 'Production Mode' },
+          { request: 'Fetch Pages' },
         ],
       },
       {
