a1b15178b88432b4beda959dcf84dfb41b8adeb9
r.ehdals <kangdongmandoo@gmail.com>
2025-09-15 19:47:34 +0900

    docs(blog): "why file-based routing" post

--- blog.config.ts
+++ blog.config.ts
@@ -5,27 +5,31 @@ export interface Post {
 
 export default [
   {
-    title: 'Introducing 4.0!',
-    date: '25-06-22',
+    title: "Why file-based routing?",
+    date: "25-09-15",
   },
   {
-    title: 'Serpack v0.1.31',
-    date: '25-06-07',
+    title: "Introducing 4.0!",
+    date: "25-06-22",
   },
   {
-    title: 'New Compiler',
-    date: '25-01-13',
+    title: "Serpack v0.1.31",
+    date: "25-06-07",
   },
   {
-    title: 'Breaking Change: zely@4!',
-    date: '24-12-01',
+    title: "New Compiler",
+    date: "25-01-13",
   },
   {
-    title: 'Introducing 3.0!',
-    date: '24-02-27',
+    title: "Breaking Change: zely@4!",
+    date: "24-12-01",
   },
   {
-    title: 'Introducing 2.0!',
-    date: '23-12-15',
+    title: "Introducing 3.0!",
+    date: "24-02-27",
+  },
+  {
+    title: "Introducing 2.0!",
+    date: "23-12-15",
   },
 ] satisfies Post[];
